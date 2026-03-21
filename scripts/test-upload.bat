ba801c088aeefe4842714655ea1ce0331a46e71a
shenzihan666 <zihanzixin@gmail.com>
2026-03-22 00:31:36 +0800

    Commit remaining local changes

--- scripts/test-upload.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-REM Run upload API tests
-cd /d "%~dp0..\backend"
-uv run pytest tests/api/v1/test_upload.py -v --tb=short %*
--- scripts/test-upload.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-# Run upload API tests
-cd "$(dirname "$0")/../backend"
-uv run pytest tests/api/v1/test_upload.py -v --tb=short "$@"
